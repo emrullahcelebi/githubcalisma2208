4e9a94d48e37804c2e5e0642395410c2855cc6e7
Java project <java100class@gmail.com>
2022-08-22 16:33:51 +0300

    a2

--- C01.java
+++ C01.java
@@ -1,7 +1,7 @@
 public class C01 {
     public static void main(String[] args) {
         System.out.println("22.08.2022 github calisma");
-
+        System.out.println("değişim saati");
 
         //sezer
 
