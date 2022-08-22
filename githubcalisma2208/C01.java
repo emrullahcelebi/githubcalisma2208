d5de228adfa4fb470f5fb18553903fc6b00e4878
Emrullah Celebi <cumali20182019@gmail.com>
2022-08-22 15:53:32 +0200

    v04

--- githubcalisma2208/C01.java
+++ githubcalisma2208/C01.java
@@ -1,8 +1,10 @@
+package githubcalisma2208;
+
 public class C01 {
     public static void main(String[] args) {
         System.out.println("22.08.2022 github calisma");
         System.out.println("yeni eklenen 01");
-        System.out.println("yeni eklenen 02");
+        System.out.println("yeni eklenen 00002");
         System.out.println("yeni eklenen 03");
         //sezer
 
