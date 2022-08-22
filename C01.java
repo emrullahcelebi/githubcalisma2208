2263e77d209f89e64adbf87be5aa7af7e2ac3169
Mehmet Emin Çatal <mhmtmnctl20@gmail.com>
2022-08-22 16:31:32 +0300

    emin

---
+++ C01.java
@@ -0,0 +1,23 @@
+public class C01 {
+    public static void main(String[] args) {
+        System.out.println("22.08.2022 github calisma");
+
+
+        //sezer
+
+
+        //muhammet
+
+
+
+        //mehmet emin
+
+
+
+        //fatih
+
+
+        //ersin
+
+    }
+}
